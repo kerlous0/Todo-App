63d7d737c88850ab78f443508e3234ec1837a69b
kerlous0 <kerlousnasser0@gmail.com>
2024-02-13 04:41:12 +0200

    edit & points & styles & rebuild

--- src/App.js
+++ src/App.js
@@ -1,47 +1,158 @@
 import { useState } from 'react';
 import './App.css';
 import Modal from './components/Modal/Modal';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faPen} from '@fortawesome/free-solid-svg-icons'
+import dayjs from 'dayjs';
+
 
 function App() {
   const [todo, setTodo] = useState([]);
+  const [id , seId] = useState(0);
+  const [modal, setModal] = useState(false);
+  const [todoName, setTodoName] = useState("");
+  const [from, setFrom] = useState(dayjs());
+  const [to, setTo] = useState(dayjs());
+  const [realFrom, setRealFrom] = useState(dayjs().format('h:mm A'));
+  const [reaTo, setRealTo] = useState(dayjs().format('h:mm A'));
+  const [edit, setEdit] = useState(false);
+  const [index, seIndex] = useState(0);
+  const [points, setPoints] = useState(0);
   // kero
+
+  const point = () => {
+    setPoints(points + 10);
+  };
 
   const completeTodo = (id) => {
     const complete = todo.filter((el) => {
       return(
-        el.name + el.from + el.to !== id
+        el.id !== id
       )
     })
     setTodo(complete)
+    point()
+
   }
+
+  const toggleModal = () => {
+    setModal(!modal)
+    setTodoName("")
+    setFrom(dayjs())
+    setTo(dayjs())
+    setRealFrom(dayjs().format('h:mm A'))
+    setRealTo(dayjs().format('h:mm A'))
+  };
+
+  const handleAdd = () => {
+    seId(id + 1)
+      const data = {
+          name: todoName,
+          from: from,
+          to: to,
+          realFrom: realFrom,
+          realTo: reaTo,
+          id: id
+      }
+      
+      setTodo([...todo, data])
+      toggleModal()
+      setTodoName("")
+      setFrom(dayjs())
+      setTo(dayjs())
+      setRealFrom(dayjs().format('h:mm A'))
+      setRealTo(dayjs().format('h:mm A'))
+  }
+
+  const handleFrom = (newValue) => {
+      setFrom(newValue)
+      setRealFrom(newValue.format('h:mm A'))
+  };
+
+  const handleTo = (newValue) => {
+      setTo(newValue)
+      setRealTo(newValue.format('h:mm A'))
+  };
+
+
+  const openEdit = (id) => {
+    setEdit(true);
+    const index = todo.findIndex(item => item.id === id);
+    seIndex(index);
+    toggleModal();
+    setTodoName(todo[index].name);
+    setFrom(todo[index].from);
+    setTo(todo[index].to);
+    setRealFrom(todo[index].realFrom);
+    setRealTo(todo[index].realTo);
+  };
+
+  const editodo = () => {
+    const updatedTodo = {
+      ...todo[index],
+      name: todoName,
+      from: from,
+      to: to,
+      realFrom: realFrom,
+      realTo: reaTo,
+      // id remains the same
+    };
+
+    console.log(index, updatedTodo);
+
+    // Create a new todo array with the updated item
+    const newTodo = [...todo];
+    newTodo[index] = updatedTodo;
+
+    // Update the todo state with the new todo array
+    setTodo(newTodo);
+
+    // Assuming toggleModal() closes the modal and resets the form
+    toggleModal();
+    setTodoName("");
+    setFrom(dayjs());
+    setTo(dayjs());
+    setRealFrom(dayjs().format('h:mm A'));
+    setRealTo(dayjs().format('h:mm A'));
+    setEdit(false);
+  };
+
 
   const todoData = todo.map((el)=> {
     return (
-      <div className="listgroupitem " key={el.name + el.from + el.to}>
+      <div className="listgroupitem w-50" key={el.id}>
         <p className="text-center mb-5">{el.name}</p>
         <div className="d-flex justify-content-around">
-          <p>from: {el.from}</p>
-          <p>to: {el.to}</p>
+          <p>from: {el.realFrom}</p>
+          <p>to: {el.realTo}</p>
         </div>
-        <p className="right" onClick={()=>completeTodo(el.name + el.from + el.to)}>✔️</p>
+        <p className="right" onClick={()=>completeTodo(el.id)}>✔️</p>
+        <FontAwesomeIcon className="left" icon={faPen} onClick={()=>openEdit(el.id)} />
       </div>
     )
   })
 
   return (
-    <div className="d-flex container justify-content-center  vh-100">
+    <div className="container">
+ 
+    <div className="d-fdlex  justify-conternt-center  ">
 
-      <div className="d-flex flex-direction: row justify-content-center w-50 ">
+    <p className="d-flex fs-4 align-items-start ">points: {points}</p>
+      <div className="d-flex flex-column justify-content-center">
 
-        <Modal setTodo={setTodo} todo={todo} />
+         
+        <button className="w-50 btn btn-warning mt-5 m-auto " onClick={toggleModal}>Add Todo</button>
+        <Modal editodo={editodo} edit={edit} toggleModal={toggleModal} handleAdd={handleAdd} handleFrom={handleFrom} handleTo={handleTo} modal={modal} todoName={todoName} setTodoName={setTodoName} from={from} to={to} />
+        
 
-        <div className="list-group mt-5 gap-4">
+        <div className="list-group justify-content-center mt-5 gap-4">
 
           {todoData}
           
         </div>
       </div>
 
+    </div>
     </div>
   );
 }
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,58 +1,18 @@
-import { useState } from "react";
 import './Modal.css'
 import * as React from 'react';
-import dayjs from 'dayjs';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import TextField from '@mui/material/TextField';
 
-export default function Modal({setTodo, todo}) {
-    const [modal, setModal] = useState(false);
-    const [todoName, setTodoName] = useState("");
-    const [from, setFrom] = useState(dayjs());
-    const [to, setTo] = useState(dayjs());
-    const [realFrom, setRealFrom] = useState(dayjs().format('h:mm A'));
-    const [reaTo, setRealTo] = useState(dayjs().format('h:mm A'));
+export default function Modal({editodo, edit, toggleModal, handleAdd, handleFrom, handleTo, modal, todoName, setTodoName, from, to}) {
+ 
 
-    const toggleModal = () => {
-        setModal(!modal)
-        setTodoName("")
-        setFrom(dayjs())
-        setTo(dayjs())
-        setRealFrom(dayjs().format('h:mm A'))
-        setRealTo(dayjs().format('h:mm A'))
-    };
-
-    const handleAdd = () => {
-        const data = {
-            name: todoName,
-            from: realFrom,
-            to: reaTo
-        }
-        setTodo([...todo, data])
-        toggleModal()
-        setTodoName("")
-        setFrom(dayjs())
-        setTo(dayjs())
-        setRealFrom(dayjs().format('h:mm A'))
-        setRealTo(dayjs().format('h:mm A'))
-    }
-
-    const handleFrom = (newValue) => {
-        setFrom(newValue)
-        setRealFrom(newValue.format('h:mm A'))
-      };
-
-    const handleTo = (newValue) => {
-        setTo(newValue)
-        setRealTo(newValue.format('h:mm A'))
-      };
+    
 
     return(
         
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <button className="btn btn-warning mt-5 m-auto " onClick={toggleModal}>Add Todo</button>
 
             {modal && (
             <div className="m">
@@ -61,18 +21,18 @@ export default function Modal({setTodo, todo}) {
                         <h2 className="text-center">Todo</h2>
                         <div className="divInput">
                             
-                            <TextField required id="outlined-basic" className="input" label="Todo Name" variant="outlined" value={todoName} onChange={(event) => setTodoName(event.target.value)} />
+                            <TextField autoFocus   required id="outlined-basic" className="input" label="Todo Name" variant="outlined" value={todoName} onChange={(event) => setTodoName(event.target.value)} />
                         </div>
                         <div className="divInput">
              
-                            <TimePicker label="Start Time" id="From" className="input" value={from} onChange={handleFrom} />
+                            <TimePicker label="Start Time" id="From" className="input"   value={from} onChange={handleFrom} />
             
                         </div>
                         <div className="divInput">
                             <TimePicker label="End Time" id="To" className="input" value={to} onChange={handleTo} />
                         </div>
                         <div className="sumCan d-flex justify-content-around">
-                            {todoName&&<button type="submit" className="Add" onClick={()=>handleAdd()}>Add</button>}
+                            {todoName&&<button type="submit" className="Add" onClick={edit ? editodo : handleAdd}>{edit ? "Edit" : "Add"}</button>}
                             <button className="Cancel" onClick={toggleModal}>Cancel</button>
                         </div>
                     
